78a1cf6473ed3c617ef5468a8b73d950734e2cb5
llauragouveia <138789825+llauragouveia@users.noreply.github.com>
2024-03-04 12:20:58 -0300

    9

---
+++ js/page.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const loginForm = document.getElementById("login-form");
+
+    loginForm.addEventListener("submit", function(event) {
+        event.preventDefault();
+        const email = loginForm.email.value;
+        const password = loginForm.password.value;
+
+
+        if (email === "usuario@gmail.com" && password === "123456") {
+            window.location.href = "./pages/page.html";
+        } else {
+            alert("Credenciais inválidas. Por favor, tente novamente.");
+        }
+    });
+});
